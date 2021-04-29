665d2533dac572e812c952ee7a224cdfab8c7db1
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2021-04-29 09:44:07 -0700

    :memo:

--- src/hero/errors/invalid-damage-formula-error.ts
+++ src/hero/errors/invalid-damage-formula-error.ts
@@ -1,3 +1,6 @@
+/**
+ * Raised when an invalid damage roll formula is detected.
+ */
 export class InvalidDamageFormulaError extends Error {
   constructor(formula: string) {
     super(`Invalid damage formula: ${formula}`)
