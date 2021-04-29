c4ee17a09b6783cfb678b3346d4f3dc95a78a141
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2021-04-29 09:39:21 -0700

    :memo: Document top-level damage roll functions

--- src/hero/hero.ts
+++ src/hero/hero.ts
@@ -4,10 +4,23 @@ import { DamageType } from './damage-type'
 
 const damageRollPattern = /^\d*(\.5|½)?d6([+-]1)?[kn]?$/i
 
+/**
+ * Rolls damage according to the Hero system based on the `formula` given.
+ *
+ * Hero damage rolls are listed like fairly normal dice rolls with one major
+ * difference. Since there are two different ways of calculating damage in the
+ * Hero system, one has to specify which type of damage the roll is for. This
+ * is often done by including an `N` (for normal damage) or `K` (for killing
+ * damage) at the end. If neither is specified, normal damage is often assumed,
+ * so that is how this function will interpret it.
+ *
+ * @param formula A Hero damage roll formula
+ * @returns Amount of damage rolled
+ */
 export function rollDamage(formula: string): DamageResult {
   const trimmed = formula.trim()
 
-  if (!validateDamageRoll(trimmed)) {
+  if (!_validateDamageRoll(trimmed)) {
     throw new InvalidDamageFormulaError(trimmed)
   }
 
@@ -18,6 +31,12 @@ export function rollDamage(formula: string): DamageResult {
   }
 }
 
-export function validateDamageRoll(formula: string): boolean {
+/**
+ * Validates the supplied damage roll `formula`.
+ *
+ * @param formula A Hero damage roll formula to validate
+ * @returns `true` if the formula is a valid Hero damage roll; `false` otherwise
+ */
+export function _validateDamageRoll(formula: string): boolean {
   return formula.match(damageRollPattern) !== null
 }
