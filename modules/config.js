9c7fd8543215450309ec32e17452069cc53e473e
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2021-04-30 15:16:09 -0700

    Prep for more complex skills

--- modules/config.js
+++ modules/config.js
@@ -1,1 +1,16 @@
 export const hero6e = {}
+
+hero6e.skillRollCharacteristics = {
+  str: "hero6e.characteristics.str",
+  dex: "hero6e.characteristics.dex",
+  con: "hero6e.characteristics.con",
+  int: "hero6e.characteristics.int",
+  ego: "hero6e.characteristics.ego",
+  pre: "hero6e.characteristics.pre"
+}
+
+hero6e.skillRollTypes = {
+  characteristic: "hero6e.skill.rollTypes.characteristic",
+  none: "hero6e.skill.rollTypes.none",
+  raw: "hero6e.skill.rollTypes.raw"
+}
--- modules/sheets/hero-item-sheet.js
+++ modules/sheets/hero-item-sheet.js
@@ -1,4 +1,11 @@
 export default class HeroItemSheet extends ItemSheet {
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      height: 340,
+      classes: ["hero6e", "sheet", "item"]
+    })
+  }
+
   get template() {
     console.log(`hero6e | Returning template: systems/hero6e/templates/sheets/${this.item.type}-sheet.html`)
 
