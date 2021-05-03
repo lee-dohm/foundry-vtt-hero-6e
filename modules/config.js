cbd299f0a21e79b1a6cb0838d51d4a9933a2525b
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2021-05-03 15:34:19 -0700

    Fix some mismatched values

--- modules/config.js
+++ modules/config.js
@@ -1,5 +1,14 @@
 export const hero6e = {}
 
+hero6e.skillRollCharacteristics = {
+  str: "STR",
+  dex: "DEX",
+  con: "CON",
+  int: "INT",
+  ego: "EGO",
+  pre: "PRE"
+}
+
 hero6e.skillRollTypes = {
   characteristic: "hero6e.skill.rollTypes.characteristic",
   none: "hero6e.skill.rollTypes.none",
