1d703a8be887ebc58cf7e222d0e3fcd7b2827ad0
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2021-05-10 13:25:25 -0700

    Reformat the Hero configuration object definition

--- modules/config.js
+++ modules/config.js
@@ -1,24 +1,18 @@
-import { rollKillingDamage, rollNormalDamage } from './damage.js'
-
-export const hero6e = {}
-
-hero6e.damage = {
-  rollNormal: rollNormalDamage,
-  rollKilling: rollKillingDamage,
-  stunMultFormula: '1d3'
-}
-
-hero6e.skillRollCharacteristics = {
-  str: 'STR',
-  dex: 'DEX',
-  con: 'CON',
-  int: 'INT',
-  ego: 'EGO',
-  pre: 'PRE'
-}
-
-hero6e.skillRollTypes = {
-  characteristic: 'hero6e.skill.rollTypes.characteristic',
-  none: 'hero6e.skill.rollTypes.none',
-  raw: 'hero6e.skill.rollTypes.raw'
+export const hero6e = {
+  damage: {
+    stunMultFormula: '1d3'
+  },
+  skillRollCharacteristics: {
+    str: 'STR',
+    dex: 'DEX',
+    con: 'CON',
+    int: 'INT',
+    ego: 'EGO',
+    pre: 'PRE'
+  },
+  skillRollTypes: {
+    characteristic: 'hero6e.skill.rollTypes.characteristic',
+    none: 'hero6e.skill.rollTypes.none',
+    raw: 'hero6e.skill.rollTypes.raw'
+  }
 }
