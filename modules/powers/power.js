ae55d15166020e99c8974906dd5430b727a24994
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2021-05-12 22:33:18 -0700

    :memo:

--- modules/powers/power.js
+++ modules/powers/power.js
@@ -1,5 +1,36 @@
 /**
  * Describes a power.
+ *
+ * A power consists of:
+ *
+ * * `name` -- Name to be displayed when referring to the power
+ * * `description` -- Full description of the power
+ * * `baseCost` -- Base cost of the power in character points
+ * * `endCost` -- Cost to use the power in `END`, if any
+ * * `adders` -- List of adders applied to the power (see below)
+ * * `advantages` -- List of advantages applied to the power (see below)
+ * * `limitations` -- List of limitations applied to the power (see below)
+ *
+ * An adder (see **6E1 313**) adds raw character points to the point cost of a power:
+ *
+ * * `cost` -- Cost of the adder in character points
+ * * `description` -- Description of the adder
+ *
+ * An advantage (see **6E1 313**) multiplies the base cost of the power after any adders
+ * are applied:
+ *
+ * * `quarters` -- Number of 1/4ths to create the multiplier of the advantage, _ex:_ 3
+ *   `quarters` would be a +3/4 advantage.
+ * * `multiplier` (calculated) -- Text representation of `quarters` for display
+ * * `description` -- Description of the advantage
+ *
+ * A limitation (see **6E1 364**) divides the active cost of the power to obtain the real
+ * cost:
+ *
+ * * `quarters` -- Number of 1/4ths to create the divisor of the limitation, _ex:_ 5
+ *   `quarters` would be a -1 1/4th limitation.
+ * * `divisor` (calculated) -- Text representation of `quarters` for display
+ * * `description` -- Description of the limitation
  */
 export default class Power {
   constructor({name, description, baseCost, endCost, adders, advantages, limitations}) {
@@ -15,7 +46,9 @@ export default class Power {
   /**
    * The active cost of the power.
    *
-   * The active cost is the base cost plus all adders and advantages.
+   * The active cost is the base cost plus all adders and advantages. Also used as the
+   * basis for a number of other things like the effect of **Drain** or calculating the
+   * base `endCost`.
    */
   get activeCost() {
     let cost = this.baseCost
