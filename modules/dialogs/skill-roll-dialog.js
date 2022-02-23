a394b44f6ee666a387663bd44c7cd6a18bcf9c24
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2022-02-23 14:53:14 -0800

    Add SkillRollDialog for future improvement of skill and characteristic rolls

---
+++ modules/dialogs/skill-roll-dialog.js
@@ -0,0 +1,14 @@
+/**
+ * Displays a dialog for performing a standard skill or characteristic roll.
+ */
+export class SkillRollDialog extends Dialog {
+  /**
+   * Constructions the dialog prior to rendering.
+   *
+   * @param {Object} data Configuration options for the dialog.
+   * @param {DialogOptions} options Dialog rendering options.
+   */
+  constructor(data, options) {
+    super(data, options)
+  }
+}
